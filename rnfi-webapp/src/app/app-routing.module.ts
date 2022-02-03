c9b462139e213f860852a5c987aaedc3e399293a
Saurav Kumar <saurav97531@gmail.com>
2022-02-03 16:31:34 +0530

    routing

--- rnfi-webapp/src/app/app-routing.module.ts
+++ rnfi-webapp/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { AuthGuard } from './guards/auth.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BankAccountFormComponent } from './components/bank-account-form/bank-account-form.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { OtpVerificationFormComponent } from './components/otp-verification-form/otp-verification-form.component';
+import { LoginService } from './guards/login.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginFormComponent},
+  {path: '', redirectTo: 'login', pathMatch: 'full' },
+  {path: 'otp-verification', component: OtpVerificationFormComponent},
+  {path: 'dashboard', component: DashboardComponent,
+    canActivate:[AuthGuard]
+  },
+  {path: 'account-form', component: BankAccountFormComponent,
+    canActivate:[AuthGuard]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- rnfi-webapp/src/app/app.module.ts
+++ rnfi-webapp/src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { BankAccountFormComponent } from './components/bank-account-form/bank-account-form.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OtpVerificationFormComponent } from './components/otp-verification-form/otp-verification-form.component';
+import { httpInterceptorProviders } from './interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    BankAccountFormComponent,
+    LoginFormComponent,
+    OtpVerificationFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [httpInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
